d287bc313a43beef215685962c4b1aa01fd3391d
Jonathan Jin <jjin082693@gmail.com>
2015-01-25 22:13:37 -0600

    emacs: bindings

--- prefs/evil.el
+++ prefs/evil.el
@@ -13,9 +13,9 @@
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
     "w" 'save-buffer
-    "q" 'save-buffers-kill-terminal
     "bk" 'kill-current-buffer
     "bn" 'next-buffer
+
     "rtw" 'delete-trailing-whitespace
 
     "ci" 'evilnc-comment-or-uncomment-lines
@@ -23,18 +23,31 @@
     "cc" 'evilnc-copy-and-comment-lines
     "cp" 'evilnc-comment-or-uncomment-paragraphs
     "cv" 'evilnc-toggle-invert-comment-line-by-line
-     
+
     "ar" 'align-regexp
 
     "gs" 'magit-status
-    "pl" 'package-list-packages)
+    "gb" 'magit-blame-mode
+
+    "pl" 'package-list-packages
+
+    "pp" 'projectile-switch-project
+    "pf" 'projectile-find-file
+    "pF" 'projectile-find-file-in-known-projects)
 
 (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
 (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
 
-(evil-mode 1)
-
 (global-evil-search-highlight-persist t)
+
 (define-key evil-normal-state-map (kbd "C-l") 'evil-search-highlight-persist-remove-all)
+(define-key evil-normal-state-map (kbd "M-p") 'evil-paste-pop)
+(define-key evil-normal-state-map (kbd "L") 'evil-end-of-line)
+(define-key evil-normal-state-map (kbd "H") 'evil-beginning-of-line)
+
 
 (setq evil-insert-state-modes (cons 'git-commit-mode evil-insert-state-modes))
+
+(evilem-default-keybindings "SPC")
+
+(evil-mode 1)
