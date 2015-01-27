220107070f3b389ff47207016377bac154e352d7
Jonathan Jin <jjin082693@gmail.com>
2015-01-27 09:02:47 -0600

    cleanup

--- prefs.el
+++ prefs.el
@@ -9,29 +9,23 @@
 (defvaralias 'js-indent-level 'tab-width)
 
 (global-auto-revert-mode)
-(when (>= emacs-major-version 24)
-  (progn
-      (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-      (add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/")
-      (add-to-list 'custom-theme-load-path
-		   "~/.emacs.d/elpa/highlight-indentation-0.5.0/")))
 
 (load-theme 'gotham t)
 
 (setq scss-compile-at-save nil)
 
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "google-chrome-beta")
+    browse-url-generic-program "google-chrome-beta")
 
 (when (fboundp 'global-linum-mode)
-  (setq linum-format 'dynamic)
-  (global-linum-mode 1))
+    (setq linum-format 'dynamic)
+    (global-linum-mode 1))
 
 (when (fboundp 'global-hl-line-mode)
-  (global-hl-line-mode 1))
+    (global-hl-line-mode 1))
 
 (when (fboundp 'column-number-mode)
-  (column-number-mode 1))
+    (column-number-mode 1))
 
 (show-paren-mode 1)
 (autopair-global-mode)
@@ -58,9 +52,6 @@
 (add-hook 'LaTeX-mode-hook 'fic-ext-mode)
 (add-hook 'LaTeX-mode-hook (lambda () (TeX-fold-mode 1)))
 
-(add-hook 'actionscript-mode-hook 'auto-fill-mode)
-(add-hook 'actionscript-mode-hook 'fic-ext-mode)
-
 (add-hook 'markdown-mode-hook 'auto-fill-mode)
 (add-hook 'markdown-mode-hook 'fic-ext-mode)
 
@@ -77,14 +68,7 @@
 (setq TeX-parse-self t)
 
 (add-hook 'comint-output-filter-functions
-	  'comint-watch-for-password-prompt)
-
-;; Set transparency of emacs
-(defun set-transparency (value)
-  "Sets the transparency of the frame window. 0=transparent/100=opaque"
-  (interactive "nTransparency Value (0 - 100 opaque): ")
-  (set-frame-parameter (selected-frame) 'alpha value))
-(set-transparency 80)
+    'comint-watch-for-password-prompt)
 
 (setq font-face-main "DejaVu Sans Mono")
 (setq font-size-small "10")
@@ -92,27 +76,12 @@
 (setq font-setting-bigger (format "%s-%s" font-face-main font-size-bigger))
 (setq font-setting-small (format "%s-%s" font-face-main font-size-small))
 
-;; MODE LINE
-(defun scale-text (cutoff-w cutoff-h)
-  (interactive "nCutoff width: \nnCutoff height: ")
-  (when (display-graphic-p nil)
-    (set-face-attribute 'mode-line nil :font font-face-main)
-    (set-face-attribute 'mode-line nil :height 100) ; FIXME: scale this too
-    (cond ((> (x-display-pixel-height) (x-display-pixel-width))
-           (if (> (x-display-pixel-width) cutoff-w)
-               (set-face-attribute 'default nil :font font-setting-bigger)
-             (set-face-attribute 'default nil :font font-setting-small)))
-          ((> (x-display-pixel-width) (x-display-pixel-height))
-           (if (> (x-display-pixel-height) cutoff-h)
-               (set-face-attribute 'default nil :font font-setting-bigger)
-             (set-face-attribute 'default nil :font font-setting-small))))))
-
 (scale-text 1920 1080)
 
 (eval-after-load "tex" 
-  '(setcdr (assoc "LaTeX" TeX-command-list)
-          '("%`%l%(mode) -shell-escape%' %t"
-	    TeX-run-TeX nil (latex-mode doctex-mode) :help "Run LaTeX")))
+    '(setcdr (assoc "LaTeX" TeX-command-list)
+         '("%`%l%(mode) -shell-escape%' %t"
+              TeX-run-TeX nil (latex-mode doctex-mode) :help "Run LaTeX")))
 
 (setq c-block-comment-prefix "* ")
 
@@ -120,3 +89,13 @@
 (setq scroll-margin 3)
 
 (setq helm-M-x-fuzzy-match t)
+
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
+(defconst bg-color "black")
+(defconst default-font "Terminus 12")
+(defconst linum-mode-excludes
+    '(doc-view-mode magit-mode)
+    "List of major modes preventing linum to be enabled in the buffer.")
--- prefs/defaults.el
+++
@@ -1,9 +0,0 @@
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message "")
-
-(defconst bg-color "black")
-(defconst default-font "Terminus 12")
-(defconst linum-mode-excludes
-    '(doc-view-mode magit-mode)
-    "List of major modes preventing linum to be enabled in the buffer.")
