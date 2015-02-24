c1e22c0c23114f2ac8e3cd9e8f7666607901205d
Jonathan Jin <jjin082693@gmail.com>
2015-02-23 23:04:50 -0600

    hydra: comments

--- init/init-keybinding.el
+++ init/init-keybinding.el
@@ -63,11 +63,7 @@
 
             "rtw" 'delete-trailing-whitespace
 
-            "ci" 'evilnc-comment-or-uncomment-lines
-            "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
-            "cc" 'evilnc-copy-and-comment-lines
-            "cp" 'evilnc-comment-or-uncomment-paragraphs
-            "cv" 'evilnc-toggle-invert-comment-line-by-line
+            "c" 'hydra-comments/body
 
             "ar" 'align-regexp
 
