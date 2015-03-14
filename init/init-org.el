c2f6c391a8b2fe6dd37f7b72caae0e605e5ebbb8
Jonathan Jin <jjin082693@gmail.com>
2015-03-14 13:16:20 -0500

    init-org: consolidate setq

--- init/init-org.el
+++ init/init-org.el
@@ -5,50 +5,25 @@
 (require 'ox-latex)
 
 (setq
+  org-agenda-files '("~/agenda")
 
+  org-latex-create-formula-image-program 'imagemagick
+  org-latex-listings 'minted
+  org-tags-column -80
   org-enforce-todo-dependencies           t
   org-enforce-todo-checkbox-dependencies  t
   org-pretty-entities                     t
   org-src-fontify-natively                t
-  org-alphabetical-lists                  t)
-
-(setq org-todo-keywords
-    '((sequence "TODO(t)" "IN-PROGRESS(r)" "|"  "DONE(d)")))
-
-(setq org-todo-keyword-faces
-    '(("TODO" . org-todo) ("IN-PROGRESS" . "yellow") ("DONE" . org-done)))
-
-;; (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
-
-(setq org-latex-pdf-process
-    '("pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f"
-         "pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f" 
-         "pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f"))
-
-(add-hook 'org-mode-hook
-    (lambda ()
-        (add-to-list 'org-structure-template-alist
-            '("C" "#+BEGIN_COMMENT\n?\n#+END_COMMENT" ""))
-        (fic-mode)))
-
-(setq
-  org-tags-column -80
-  org-latex-create-formula-image-program 'imagemagick
-  org-latex-listings 'minted)
-
-(add-to-list 'org-latex-packages-alist '("" "minted"))
-
-(defun my-org-autodone (n-done n-not-done)
-    "Switch entry to DONE when all subentries are done, to TODO otherwise."
-    (let (org-log-done org-log-states)   ; turn off logging
-        (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
-
-(add-hook 'org-after-todo-statistics-hook 'my-org-autodone)
-
-(setq org-agenda-files '("~/agenda"))
-
-(setq org-agenda-custom-commands
-  '(("j" "Homework"
+  org-alphabetical-lists                  t
+  
+  org-todo-keywords
+  '((sequence "TODO(t)" "IN-PROGRESS(r)" "|"  "DONE(d)"))
+  
+  org-todo-keyword-faces
+  '(("TODO" . org-todo) ("IN-PROGRESS" . "yellow") ("DONE" . org-done))
+  
+  org-agenda-custom-commands
+  '(("h" "Homework"
       ((agenda "" ((org-agenda-ndays 14)
                     (org-agenda-start-on-weekday nil)
                     (org-agenda-prefix-format " %-12:c%?-12t% s")))
@@ -57,6 +32,28 @@
      
      ("r" "Reading"
        ((tags-todo "CATEGORY=\"Reading\""
-          ((org-agenda-prefix-format "%:T ")))))))
+          ((org-agenda-prefix-format "%:T "))))))
+  
+  org-latex-pdf-process
+  '("pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f"
+     "pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f" 
+     "pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+;; (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
+
+(add-hook 'org-mode-hook
+  (lambda ()
+    (add-to-list 'org-structure-template-alist
+      '("C" "#+BEGIN_COMMENT\n?\n#+END_COMMENT" ""))
+    (fic-mode)))
+
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+
+(defun my-org-autodone (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'my-org-autodone)
 
 ;;; init-org.el ends here
