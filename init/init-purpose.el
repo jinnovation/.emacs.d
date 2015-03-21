ec08bf7854da1b05697b5d6f911fdca78ed49a39
Jonathan Jin <jjin082693@gmail.com>
2015-03-20 20:42:04 -0500

    init-purpose + purpose-layout: init

---
+++ init/init-purpose.el
@@ -0,0 +1,16 @@
+;;; init-purpose.el --- configs for purpose.el
+
+(provide 'init-purpose)
+
+(purpose-x-magit-single-on)
+
+(setq purpose-user-mode-purposes
+  '((prog-mode . edit)))
+
+(setq purpose-user-regexp-purposes
+  '(("mu4e" . admin)
+     ("elfeed" . admin)))
+
+(purpose-compile-user-configuration)
+
+;;; init-purpose.el ends here
