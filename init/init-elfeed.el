c2c2d26226c2831bcd27214036d1c175fd9884fd
Jonathan Jin <jjin082693@gmail.com>
2015-03-22 22:42:22 -0500

    elfeed: add tags

--- init/init-elfeed.el
+++ init/init-elfeed.el
@@ -6,7 +6,7 @@
   '(("https://news.ycombinator.com/rss" software news)
      ("http://www.aljazeera.com/Services/Rss/?PostingId=2007731105943979989" news)
      ("http://ny.curbed.com/atom.xml" realestate news)
-     ("http://www.avclub.com/feed/rss/" film news)
+     ("http://www.avclub.com/feed/rss/" film entertainment news)
      ("http://fivethirtyeight.com/all/feed")
      ("http://www.tor.com/rss/frontpage_full" literature)
      ("http://longform.org/feed.rss")
@@ -20,10 +20,10 @@
 
      ("http://feeds.99percentinvisible.org/99percentinvisible" podcast design)
 
-     ("http://rss.escapistmagazine.com/news/0.xml" entertainment)
-     ("http://rss.escapistmagazine.com/videos/list/1.xml" entertainment)
-     ("http://www.engadget.com/tag/@gaming/rss.xml" entertainment)
-     ("http://feeds.feedburner.com/RockPaperShotgun" entertainment)
+     ("http://rss.escapistmagazine.com/news/0.xml" entertainment videogames)
+     ("http://rss.escapistmagazine.com/videos/list/1.xml" entertainment videogames)
+     ("http://www.engadget.com/tag/@gaming/rss.xml" entertainment videogames)
+     ("http://feeds.feedburner.com/RockPaperShotgun" entertainment videogames)
 
      ("http://feeds.feedburner.com/codinghorror" software)
      ("http://feeds.feedburner.com/thisdeveloperslife" software)
@@ -31,20 +31,20 @@
      ("http://www.joelonsoftware.com/rss.xml" software)
      ("http://onethingwell.org/rss" tech software)
      ("http://pandodaily.com.feedsportal.com/c/35141/f/650422/index.rss" tech)
-     ("https://medium.com/feed/backchannel" tech)
+     ("https://medium.com/feed/backchannel" tech software)
      ("http://feeds.feedburner.com/laptopmag" tech)
      ("http://recode.net/feed/" tech)
      ("http://recode.net/category/reviews/feed/" tech)
      ("http://feeds.feedburner.com/AndroidPolice" android tech)
-     ("http://bits.blogs.nytimes.com/feed/")
+     ("http://bits.blogs.nytimes.com/feed/" tech)
 
      ("http://www.eater.com/rss/index.xml" food)
      ("http://ny.eater.com/rss/index.xml" food ny)
 
      ("http://notwithoutsalt.com/feed/" food)
      ("http://feeds.feedburner.com/blogspot/sBff")
-     ("http://feeds.feedburner.com/nymag/Food")
-     ("http://feeds.feedburner.com/seriouseatsfeaturesvideos")
+     ("http://feeds.feedburner.com/nymag/Food" food)
+     ("http://feeds.feedburner.com/seriouseatsfeaturesvideos" food)
 
      ("http://xkcd.com/rss.xml" comic)
      ("http://feeds.feedburner.com/Explosm" comic)
