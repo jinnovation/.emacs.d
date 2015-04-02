04f96d47843249b507b5275d2e906cafe02a6081
Jonathan Jin <jjin082693@gmail.com>
2015-04-01 20:07:45 -0500

    alignment + reorg

--- init/init-keybinding.el
+++ init/init-keybinding.el
@@ -10,11 +10,16 @@
 (require 'evil-leader)
 (require 'purpose)
 
+(local-unset-key (kbd "C-a"))
+(local-unset-key (kbd "C-x C-x"))
+(local-unset-key (kbd "C-w"))
+(local-unset-key (kbd "C-l"))
+
 (global-unset-key (kbd "<menu>"))
 
-(global-set-key (kbd "M-`") 'ace-window)
+(global-set-key (kbd "M-`")                'ace-window)
 
-(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "C-<tab>")            'other-window)
 (global-set-key (kbd "\<C-S-iso-lefttab>") 'prev-window)
 
 (global-set-key (kbd "<f1>") 'dired-at-current)
@@ -30,68 +35,66 @@
 (global-set-key (kbd "C-x k") 'kill-current-buffer)
 (global-set-key (kbd "M-x")   'helm-M-x)
 
-(local-unset-key (kbd "C-a"))
-(local-unset-key (kbd "C-x C-x"))
-(local-unset-key (kbd "C-w"))
-(local-unset-key (kbd "C-l"))
-
 (define-key evil-emacs-state-map (kbd "<escape>") 'evil-execute-in-normal-state)
 
 (define-key evil-normal-state-map (kbd "C-l")
   'evil-search-highlight-persist-remove-all)
-(define-key evil-normal-state-map (kbd "L") 'evil-end-of-line)
-(define-key evil-normal-state-map (kbd "H") 'evil-beginning-of-line)
+
+(define-key evil-normal-state-map (kbd "L")     'evil-end-of-line)
+(define-key evil-normal-state-map (kbd "H")     'evil-beginning-of-line)
 
 (define-key evil-normal-state-map (kbd "C-w q") 'delete-window)
-(define-key evil-normal-state-map (kbd "RET") 'insert-newline-after)
+(define-key evil-normal-state-map (kbd "RET")   'insert-newline-after)
+
+(define-key evil-normal-state-map (kbd "C-a")   'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
 
 (define-key evil-insert-state-map (kbd "RET")
   'newline-and-indent)
-
-(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-(define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
 
 (evilem-default-keybindings "SPC")
 
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
-  "M" 'helm-M-x
-  "w" 'save-buffer
+  "M"   'helm-M-x
+  "w"   'save-buffer
 
   "rtw" 'delete-trailing-whitespace
 
-  "ci" 'evilnc-comment-or-uncomment-lines
-  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
-  "cc" 'evilnc-copy-and-comment-lines
-  "cp" 'evilnc-comment-or-uncomment-paragraphs
-  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  ;; commenting
+  "ci"  'evilnc-comment-or-uncomment-lines
+  "cl"  'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc"  'evilnc-copy-and-comment-lines
+  "cp"  'evilnc-comment-or-uncomment-paragraphs
+  "cv"  'evilnc-toggle-invert-comment-line-by-line
 
-  "ii" 'helm-circe
-  "in" 'helm-circe-new-activity
+  ;; IRC + chat
+  "ii"  'helm-circe
+  "in"  'helm-circe-new-activity
 
-  "ar" 'align-regexp
-  "/" 'helm-swoop
-  "ff" 'helm-find-files
+  "ar"  'align-regexp
+  "/"   'helm-swoop
+  "ff"  'helm-find-files
 
   ;; project
-  "pp" 'helm-projectile
-  "pf" 'helm-projectile-find-file
-  "pF" 'helm-projectile-find-file-in-known-projects
-  "pa" 'helm-projectile-ag
-  "pK" 'projectile-kill-buffers
-  "pc" 'projectile-compile-project
+  "pp"  'helm-projectile
+  "pf"  'helm-projectile-find-file
+  "pF"  'helm-projectile-find-file-in-known-projects
+  "pa"  'helm-projectile-ag
+  "pK"  'projectile-kill-buffers
+  "pc"  'projectile-compile-project
 
   ;; git + version-control
-  "gs" 'magit-status
-  "gb" 'magit-blame-mode
+  "gs"  'magit-status
+  "gb"  'magit-blame-mode
 
   ;; buffer manipulation
-  "bb" 'helm-buffers-list
-  "bk" 'kill-current-buffer
-  "bn" 'next-buffer
+  "bb"  'helm-buffers-list
+  "bk"  'kill-current-buffer
+  "bn"  'next-buffer
 
   ;; mail
-  "mu" 'mu4e-update-mail-and-index)
+  "mu"  'mu4e-update-mail-and-index)
 
 (evil-leader/set-key-for-mode 'org-mode "op" 'org-set-property)
 
