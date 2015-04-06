95c48eababc0d28cf36ac448d33767958a14717e
Jonathan Jin <jjin082693@gmail.com>
2015-04-06 13:00:46 -0500

    mu4e: ability to attach files

--- init/init-mu4e.el
+++ init/init-mu4e.el
@@ -65,4 +65,20 @@
               "jjin082693@uchicago.edu")
             (t "jonathan@jjin.me")))))))
 
+(require 'gnus-dired)
+;; make the `gnus-dired-mail-buffers' function also work on message-mode derived
+;; modes, such as mu4e-compose-mode
+(defun gnus-dired-mail-buffers ()
+  (let (buffers)
+    (save-current-buffer
+      (dolist (buffer (buffer-list t))
+        (set-buffer buffer)
+        (when (and (derived-mode-p 'message-mode)
+                (null message-sent-message-via))
+          (push (buffer-name buffer) buffers))))
+    (nreverse buffers)))
+
+(setq gnus-dired-mail-mode 'mu4e-user-agent)
+(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+
 ;;; init-mu4e.el ends here
