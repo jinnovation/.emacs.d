a21f50ba6cbe9de5457286491c3194108167c86f
Jonathan Jin <jjin082693@gmail.com>
2015-04-20 12:48:02 -0500

    doc-view: rm keybindings

--- init/init-doc-view.el
+++ init/init-doc-view.el
@@ -2,15 +2,6 @@
 
 (provide 'init-doc-view)
 
-(add-hook 'doc-view-mode-hook           ;FIXME: convert to eval-after-load
-    (lambda ()
-        (define-key doc-view-mode-map (kbd "j") 'doc-view-next-line-or-next-page)
-        (define-key doc-view-mode-map (kbd "k")
-            'doc-view-previous-line-or-previous-page)
-        (define-key doc-view-mode-map (kbd "g") 'doc-view-first-page)
-        (define-key doc-view-mode-map (kbd "G") 'doc-view-last-page)
-        (doc-view-fit-width-to-window)))
-
 (setq doc-view-resolution 200)
 
 ;;; init-doc-view.el ends here
