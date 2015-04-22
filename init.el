060e1329f8167d33cccc3d9bd7a839b8151856a8
Jonathan Jin <jjin082693@gmail.com>
2015-04-22 18:03:35 -0500

    init: delete customs

--- init.el
+++ init.el
@@ -9,14 +9,11 @@
 
 (require 'malyon)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-  '(ansi-color-faces-vector
-     [default bold shadow italic underline bold bold-italic bold])
- '(paradox-github-token t))
+(pdf-tools-install)
+
+(setq paradox-github-token t)
+(setq ansi-color-faces-vector
+  [default bold shadow italic underline bold bold-italic bold])
 
 (mapc 'require '(init-fn
                   init-keybinding
