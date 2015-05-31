3a67f34b541cc557a877751595fdfae627515a2d
Jonathan Jin <jjin082693@gmail.com>
2015-05-30 21:12:27 -0500

    README: dat literate programming config

--- init/init-doc-view.el
+++
@@ -1,5 +0,0 @@
-;;; init-doc-view.el --- Doc-view-specific configurations
-
-(provide 'init-doc-view)
-
-;;; init-doc-view.el ends here
--- init/init-markdown.el
+++
@@ -1,5 +0,0 @@
-;;; init-markdown.el --- Markdown-specific configs
-(provide 'init-markdown)
-
-
-;;; init-markdown.el ends here
