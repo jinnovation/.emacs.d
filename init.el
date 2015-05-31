1c48b23cb17533d4aca04f838698a36294fb491b
Jonathan Jin <jjin082693@gmail.com>
2015-05-30 19:53:55 -0500

    use-package: migrate more

--- init.el
+++ init.el
@@ -10,6 +10,9 @@
 (require 'use-package)
 (require 'diminish)
 
+(setq ring-bell-function 'ignore)
+(setq-default x-stretch-cursor t)
+
 (use-package ess-site
   :init
   (setq inferior-R-args "--quiet")
@@ -35,7 +38,7 @@
   (pdf-tools-install))
 
 (use-package paradox
-  :init
+  :config
   (setq paradox-github-token t))
 
 (use-package ace-window
@@ -338,6 +341,101 @@
 
     (bind-key "H-o" 'hydra-org/body org-mode-map)))
 
+(use-package mu4e
+  :commands (mu4e-main-mode
+              mu4e-view-mode
+              mu4e-about-mode
+              mu4e-headers-mode
+              mu4e-compose-mode)
+  :bind (("H-M" . mu4e)
+          ("H-m u" . mu4e-update-mail-and-index)
+          ("H-m i" . mu4e-interrupt-update-mail))
+  :init
+  :config
+  (bind-key "H-u" 'mu4e-update-mail-and-index mu4e-headers-mode-map)
+
+  (setq
+    mu4e-maildir "~/mail"
+
+    mu4e-drafts-folder "/[Gmail].Drafts"
+    mu4e-sent-folder   "/[Gmail].Sent Mail"
+    mu4e-trash-folder  "/[Gmail].Trash"
+    mu4e-refile-folder "/[Gmail].All Mail")
+
+  ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+  ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+  ;; additional non-Gmail addresses and want assign them different
+  ;; behavior.)
+  (setq mu4e-sent-messages-behavior 'delete)
+
+  ;; you can quickly switch to your Inbox -- press ``ji''
+  (setq mu4e-maildir-shortcuts
+    '(("/INBOX"               . ?i)
+       ("/[Gmail].Sent Mail"   . ?s)
+       ("/[Gmail].Trash"       . ?t)
+       ("/[Gmail].All Mail"    . ?a)))
+
+  ;; allow for updating mail using 'U' in the main view:
+  (setq mu4e-get-mail-command "offlineimap")
+
+  (setq mu4e-user-mail-address-list
+    '("jjin082693@gmail.com"
+       "jjin082693@uchicago.edu"
+       "jonathan@jjin.me"))
+
+  (defvaralias 'mu4e-compose-signature 'message-signature)
+
+  ;; don't keep message buffers around
+  (setq message-kill-buffer-on-exit t)
+
+  (setq
+    mu4e-html-renderer 'w3m
+    mu4e-html2text-command "w3m -dump -T text/html")
+
+  ;; make sure the gnutls command line utils are installed
+  ;; (require 'smtpmail)
+
+  (setq
+    message-send-mail-function 'smtpmail-send-it
+    smtpmail-stream-type 'starttls
+    smtpmail-default-smtp-server "smtp.gmail.com"
+    smtpmail-smtp-server "smtp.gmail.com"
+    smtpmail-smtp-service 587)
+
+  (add-hook 'mu4e-view-mode-hook
+    'visual-line-mode)
+
+  (add-hook 'mu4e-compose-pre-hook
+    (defun my-set-from-address ()
+      "Set the From address based on the To address of the original."
+      (let ((msg mu4e-compose-parent-message)) ;; msg is shorter...
+        (when msg
+          (setq user-mail-address
+            (cond
+              ;; TODO; pull from mu4e-user-mail-address-list
+              ((mu4e-message-contact-field-matches msg :to "jjin082693@gmail.com")
+                "jjin082693@gmail.com")
+              ((mu4e-message-contact-field-matches msg :to "jjin082693@uchicago.edu")
+                "jjin082693@uchicago.edu")
+              (t "jonathan@jjin.me")))))))
+
+  (use-package gnus-dired
+    :config
+    ;; make the `gnus-dired-mail-buffers' function also work on message-mode derived
+    ;; modes, such as mu4e-compose-mode
+    (defun gnus-dired-mail-buffers ()
+      (let (buffers)
+        (save-current-buffer
+          (dolist (buffer (buffer-list t))
+            (set-buffer buffer)
+            (when (and (derived-mode-p 'message-mode)
+                    (null message-sent-message-via))
+              (push (buffer-name buffer) buffers))))
+        (nreverse buffers)))
+
+    (setq gnus-dired-mail-mode 'mu4e-user-agent)
+    (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)))
+
 (use-package doc-view
   :init
   (setq doc-view-resolution 200))
@@ -351,8 +449,6 @@
                   init-projectile
                   init-linum
                   init-appearance
-
-                  init-mu4e
 
                   ;; languages
                   init-c
